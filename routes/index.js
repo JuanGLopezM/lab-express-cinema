aa699dcf60770c77a661c49b78b8283ac5cfcd58
Juan G Lopez <nahujg@gmail.com>
2022-06-24 18:28:56 +0200

    iteración  4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+//router.get("/movies", (req, res, next) => res.render( "movies"));
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((response) => {
+      console.log(response);
+      res.render("movies.hbs", { response });
+    })
+    .catch((error) => console.log(error));
+});
+
+//destructuracion = const {_id} = req.params
+
+router.get("/details/:id", (req, res) => {
+  const id = req.params.id;
+  Movie.findById(id)
+    .then((response) => {
+      console.log(response);
+      res.render("details.hbs", response);
+    })
+    .catch((error) => console.log(error));
+});
 
 module.exports = router;
